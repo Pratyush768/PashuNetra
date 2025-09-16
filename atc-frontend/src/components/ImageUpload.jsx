7f6f1a9cf05ba103a09b1945fbbb4a78b7404cae
Pratyush Kumar Sahoo <144750848+Pratyush768@users.noreply.github.com>
2025-09-17 01:35:17 +0530

    ImageUpload.jsx

--- atc-frontend/src/components/ImageUpload.jsx
+++ atc-frontend/src/components/ImageUpload.jsx
@@ -26,7 +26,7 @@ function CowImageUpload({ onAnalysisComplete }) {
             const formData = new FormData();
             formData.append('image', selectedImage);
 
-            const response = await fetch('http://localhost:3001/analyze-cow', {
+            const response = await fetch('https://pashu-netra-backend.vercel.app/', {
                 method: 'POST',
                 body: formData
             });
